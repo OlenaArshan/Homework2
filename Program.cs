41a88affffd0014e9f900e601e57f9f59d5fcb73
ONHN <ONHN@DK01WV6164>
2022-06-02 17:25:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentValueCalculator {
+    enum TransactionType {
+        Buy, Sell
+    }
+    class Program {
+        static void Main(string[] args) {
+            double tradePrice;
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            int nominal = Int32.Parse(userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
